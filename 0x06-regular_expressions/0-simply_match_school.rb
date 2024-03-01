06bdeae5c279f10d63824587293d7aa017f12645
Esseh12 <gbenmoese788@gmai.com>
2024-03-01 03:42:30 -0800

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,12 +1,2 @@
 #!/usr/bin/env ruby
-#Getting the argument passed to the script
-string_match = ARGV[0]
-
-#Defines the regex pattern
-regex_pattern = /School/
-
-#Matching the pattern against the string
-  result = string_match.match(regex_pattern)
-
-#Printing the matched substring
-puts result ? result[0] : ""
+puts ARGV[0].scan(/School/).join
--- 0x06-regular_expressions/1-repetition_token_0.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-#string to be matched
-string_match = ARGV[0]
-#The REGEX pattern to follow
-regex_pattern = /hbt{2, 5}n/
-#The result of the match
-result = string_match.match(regex_pattern)
-#Print to STDOUT
-puts result ? result[0] : ""
