be74b6a3b41d3d9364a4fa327a2d5270b99d627c
Esseh12 <gbenmoese788@gmail.com>
2024-05-02 23:45:37 +0100

    installed stdlib module

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+file_line { 'Turn off passwd auth':
+  ensure  => 'present',
+  path  => '/etc/ssh/ssh_config',
+  line  => ' PasswordAuthentication no',
+  replace  => true,
+}
+
+file_line { 'Identity file':
+  ensure  => 'present',
+  path  => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
+  replace => true,
+}
